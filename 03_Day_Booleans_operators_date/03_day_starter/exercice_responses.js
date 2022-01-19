c2307d49ee3c6643a1949b71e4a1596e92ebb9e4
wicaksop <pradityo.wicaksono@etu.univ-grenoble-alpes.fr>
2022-01-19 21:54:16 +0100

    exercice 3 done

---
+++ 03_Day_Booleans_operators_date/03_day_starter/exercice_responses.js
@@ -0,0 +1,14 @@
+// Create a human readable time format using the Date time object
+
+// YYYY-MM-DD HH:mm
+// DD-MM-YYYY HH:mm
+// DD/MM/YYYY HH:mm
+
+const now = new Date()
+const year = now.getFullYear() // return year
+const month = now.getMonth() + 1 // return month(0 - 11)
+const date = now.getDate() // return date (1 - 31)
+const hours = now.getHours() // return number (0 - 23)
+const minutes = now.getMinutes() // return number (0 -59)
+
+console.log(`${date}/${month}/${year} ${hours}:${minutes}`)
